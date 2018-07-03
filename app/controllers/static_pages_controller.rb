3290ce56bb20fd25ef043e1805193214353ae413
ramonpvz <ramon.pinedav@gmail.com>
2018-07-02 22:10:36 -0500

    Fixing broken links

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def about
+    puts("---------------------------------- Sending to about page...")
+  end
+
+  def post
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/homepage'
   root 'welcome#homepage'
+  get '/home', to: 'welcome#homepage'
+  get '/about', to: 'static_pages#about'
+  get '/post', to: 'static_pages#post'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
